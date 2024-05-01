7a136b62e62e335a9fe3b5e24174f2d3e0ae1831
Priyanka Aiyer <115761842+priyanka-aiyer@users.noreply.github.com>
2024-05-01 19:12:50 -0400

    Updated Controller.java

--- fin_pj/Handlers/Controller.java
+++ fin_pj/Handlers/Controller.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class Controller {
 
 	private static Connection connection;
@@ -14,15 +13,25 @@ public class Controller {
     private static PreparedStatement psUpdateUser;
     private static PreparedStatement psGetUserName;
 
+    private static PreparedStatement psCustomer;
     private static PreparedStatement psSupplier;
     private static PreparedStatement psAddress;
 
     private static ResultSet resultSet;
     private static ArrayList<User> userList = new ArrayList<>();
     private static ArrayList<Supplier> supplierList = new ArrayList<>();
+    private static ArrayList<Customer> customerList = new ArrayList<>();
 
-    //// Handlers w.r.t. User object    
-    // handler(): Add User along with a Role
+    //// Handlers w.r.t. User object  
+    /*
+    * addUser()		 
+	* updatePassword()	 	
+	* checkUserName()		 
+	* getUser()		 	
+	* getAllUsers() 
+    */
+    
+    // handler(): Add User along with a Role; such as Admin, Supplier, Customer
     public static void addUser(String username, String role, String type, String passwd, String dte) {   	
         try {
             psAddUser = connection.prepareStatement("Insert into USER (USERNAME, USER_ROLE, USER_TYPE, "
@@ -106,7 +115,15 @@ public class Controller {
         return resultsUsersList;
     }
 
-    ////Handlers w.r.t. Supplier object
+    //// Handlers w.r.t. Supplier object
+	/*
+	addSupplier() 		 
+	addSupplierAddress()	 
+	checkSupplierName()		 
+	getSupplier() 		 
+	getSupplierAddress()	 
+	getAllSuppliers()	
+	*/
     
     // handler(): Add Supplier along-with their Address
     public static void addSupplier(String supplierName, String contactFname, String contactLname, 
@@ -247,9 +264,165 @@ public class Controller {
         return resultsSuppliersList;
     }
 
+    //// Handlers w.r.t. Customer object
+	/*
+	addCustomer() 		 
+	addCustomerAddress()	 
+	checkCustomerPhone()		 
+	getCustomer() 		 
+	getCustomerAddress()	 
+	getAllCustomers()	
+	*/
+    
+    // handler(): Add Customer along-with their Address
+    public static void addCustomer(String custFname, String custLname, 
+    								String custPhone, String custEmail, Long userid) 
+    {   	
+        try {
+        	psCustomer = connection.prepareStatement("Insert into CUSTOMER (FIRST_NAME, "
+        			+ "LAST_NAME, PHONE_NUMBER, EMAIL, USER_ID) " + "values (?,?,?,?,?) ");
+        	psCustomer.setString(1, custFname);
+        	psCustomer.setString(2, custLname);
+        	psCustomer.setString(3, custPhone);
+        	psCustomer.setString(4, custEmail);
+        	psCustomer.setLong(5, userid);
+        	psCustomer.executeUpdate();
+            
+        } catch(SQLException se) {
+            System.out.println("Could not add Customer. " + se.getMessage());
+        }     
+    }
+    
+    // handler(): Add Customer Address
+    public static void addCustomerAddress(String custPhone, String street, String city, String state, String country, String zip) 
+    {   	
+       	Long customerID = null;
+        try {
+        	// get the CUST_ID
+        	psCustomer = connection.prepareStatement("Select CUST_ID from CUSTOMER where PHONE_NUMBER = ?");
+        	psCustomer.setString(1, custPhone);
+        	resultSet = psCustomer.executeQuery();
+            while(resultSet.next() == true) {
+            	customerID = resultSet.getLong("CUST_ID");                        
+            }
+            
+        	// Add the Address for that CUST_ID
+        	psAddress = connection.prepareStatement("Insert into CUST_ADDR (ADDR_STREET, ADDR_CITY, "
+        			+ "ADDR_STATE, ADDR_COUNTRY, ADDR_ZIPCODE, CUST_ID) " + "values (?,?,?,?,?,?) ");
+        	psAddress.setString(1, street);
+        	psAddress.setString(2, city);
+        	psAddress.setString(3, state);
+        	psAddress.setString(4, country);
+        	psAddress.setString(5, zip);
+        	psAddress.setLong(6, customerID);
+        	psAddress.executeUpdate();
+        } catch(SQLException se) {
+            System.out.println("Could not add Address for the specified Customer. " + se.getMessage());
+        }     
+    }
+    
+    // check if Customer-Phone Number exists
+    public static Boolean checkCustomerPhone(String custPhoneNumber) {     
+        try {
+        	psCustomer = connection.prepareStatement("Select PHONE_NUMBER from CUSTOMER where PHONE_NUMBER = ?");
+        	psCustomer.setString(1, custPhoneNumber);
+            resultSet = psCustomer.executeQuery();          
+            while(resultSet.next() == true) {
+                return true;
+            }
+        } catch(SQLException se) {
+        	System.out.println("Could not fetch the Customer Phone number. " + se.getMessage());
+        }
+        
+        return false;        
+    }    
+    
+    // handler(): gets a Customer info. (retrieved Object info can be Viewed)
+    public static Customer getCustomer(String custPhoneNumber) {   	
+    	Customer customerObj = null;
+    	
+        try {
+        	// get Customer details
+        	psCustomer = connection.prepareStatement("Select CUST_ID, FIRST_NAME, LAST_NAME, "
+        			+ "PHONE_NUMBER, EMAIL from CUSTOMER where PHONE_NUMBER = ?");
+        	psCustomer.setString(1, custPhoneNumber);
+        	resultSet = psCustomer.executeQuery();
+
+            while(resultSet.next() == true) {
+            	customerObj = new Customer(resultSet.getLong("CUST_ID"), resultSet.getString("FIRST_NAME"), 
+            			resultSet.getString("LAST_NAME"), resultSet.getString("PHONE_NUMBER"), 
+            			resultSet.getString("EMAIL"));                        
+            }
+        } catch(SQLException se) {
+            System.out.println("Could not get the specified Customer info. " + se.getMessage());
+        }  
+        
+        return customerObj;
+    }
+    
+    // handler(): gets a CustomerAddress info. (retrieved Object info can be Viewed)
+    public static CustomerAddress getCustomerAddress(String custPhoneNumber) {   	
+    	CustomerAddress customerAddressObj = null;
+        Long customerID = 0L;
+    	
+        try {
+        	// get the CUST_ID
+        	psCustomer = connection.prepareStatement("Select CUST_ID from CUSTOMER where PHONE_NUMBER = ?");
+        	psCustomer.setString(1, custPhoneNumber);
+        	resultSet = psCustomer.executeQuery();
+            while(resultSet.next() == true) {
+            	customerID = resultSet.getLong("CUST_ID");                        
+            }
+            
+            // get Customer-Address details            
+            psAddress = connection.prepareStatement("Select ADDR_STREET, ADDR_CITY, ADDR_STATE, ADDR_COUNTRY, ADDR_ZIPCODE from CUST_ADDR where CUST_ID = ?");
+            psAddress.setLong(1, customerID);
+        	resultSet = psAddress.executeQuery();
+
+            while(resultSet.next() == true) {
+            	customerAddressObj = new CustomerAddress(resultSet.getString("ADDR_STREET"), 
+            			resultSet.getString("ADDR_CITY"), resultSet.getString("ADDR_STATE"), 
+            			resultSet.getString("ADDR_COUNTRY"), resultSet.getString("ADDR_ZIPCODE") );                        
+            }
+            
+        } catch(SQLException se) {
+            System.out.println("Could not get the specified Customer info. " + se.getMessage());
+        }  
+        
+        return customerAddressObj;
+    }
+    
+    // handler(): to get the list of Customers (info may be required by an Admin)
+    public static ArrayList<Customer> getAllCustomers() {   	
+        ArrayList<Customer> resultsCustomersList = null;       
+        try {
+        	psCustomer = connection.prepareStatement("Select CUST_ID, FIRST_NAME, LAST_NAME, "
+        										+ "PHONE_NUMBER, EMAIL from CUSTOMER");
+            resultSet = psCustomer.executeQuery();
+
+            resultsCustomersList = new ArrayList<Customer>();        
+            while(resultSet.next() == true) {
+            	resultsCustomersList.add(new Customer(resultSet.getLong("CUST_ID"), 
+            			resultSet.getString("FIRST_NAME"), resultSet.getString("LAST_NAME"),
+            			resultSet.getString("PHONE_NUMBER"), resultSet.getString("EMAIL") ));
+            }
+        } catch(SQLException se) {
+            System.out.println("Could not get Supplier List. " + se.getMessage());
+        }  
+        
+        return resultsCustomersList;
+    }
+    
+    ////
+    //// Handlers w.r.t. Stock, Item, Order objects
+    ////
     
     
-    ////////////////////////////////////////
+    
+    
+    
+////////////////////////////////////////////
+
     ////////////////////////////////////////
     //// Main method(): To Test the handlers
     ////////////////////////////////////////
@@ -262,23 +435,25 @@ public class Controller {
     	connection = DBConnection.getConnection();
     	System.out.println("DB Connected is : " + connection); 
     	
-    	
-    ////// Call to test Function handlers()...     	
-        
+    //////
+    ////// Test the function handlers()...     	
+    ////// 
+    	Boolean result;
+   	
     	// Call checkUserName() and display result
-        Boolean result = Controller.checkUserName("user08");
+        result = Controller.checkUserName("user10");
         if (result == true)
         	System.out.println("Username already exist. Please try another Username");
         
     	// Call addUser()
-        Controller.addUser("user08", "Supplier", "S", "passwd8", "2024-04-30");
+        Controller.addUser("user10", "Supplier", "S", "passwd10", "2024-05-01");
         
         // Call updatePassword()
-        Controller.updatePassword("user08", "new-Pwd8");
+        Controller.updatePassword("user10", "new-Pwd10");
 		
     	// Call getUser()
         User usrObj = null;
-        usrObj = Controller.getUser("user08");
+        usrObj = Controller.getUser("user10");
         System.out.println("The Role of Username " + usrObj.getUserName() + " is : " + usrObj.getUserRole());
         
         // Call getAllUsers() and display the Users List
@@ -287,20 +462,21 @@ public class Controller {
         	System.out.println("Username in the userList : " + userList.get(i).getUserName());
         }
 
-        
-    ////// Supplier handlers().....
-        String suppName = "Lyzol";
+       
+	////// Call and Test : Supplier handlers().....        
+        String suppName = "Clorax";
         
     	// Call checkSupplier() and display result
         result = Controller.checkSupplierName(suppName);
         if (result == true)
         	System.out.println("Supplier name already exist. Please try another Supplier name");
-    
+        
+// Manually, fetch userid    
         // Call addSupplier()
-        Controller.addSupplier(suppName, "Joe", "Jonas", "7021110000", 4L); // say userid = 4
+        Controller.addSupplier(suppName, "Mark", "Jonas", "7021110000", 6L); // say userid = 38
         
         // addSupplierAddress()
-        Controller.addSupplierAddress(suppName, "400 Flatbush", "Brooklyn", "NewYork", "USA", "11202");
+        Controller.addSupplierAddress(suppName, "801 TiffanyStreet", "Brooklyn", "NewYork", "USA", "11202");
 
     	// Call getSupplier() 		
         Supplier supplierObj = null;
@@ -317,9 +493,42 @@ public class Controller {
         for (int i=0; i<supplierList.size(); i++) {
         	System.out.println("Supplier name in the supplierList : " + supplierList.get(i).getSupplierName());
         }
-       
+
+
+   	////// Call and Test : Customer handlers().....        
+        String custPhone = "7321119999";
+
+    	// Call checkCustomerPhone() and display result
+        result = Controller.checkCustomerPhone(custPhone);
+        if (result == true)
+        	System.out.println("Customer already exist. Please try another Customer Phone number.");
+        
+// Manually, fetch userid
+        // Call addCustomer()
+        Controller.addCustomer("Smith", "Scott", custPhone, "ss@ggmail.com", 6L); // say userid = 6        
+        // addCustomerAddress()
+        Controller.addCustomerAddress(custPhone, "111 Bouldyard", "Manhattan", "NewYork", "USA", "11401");
+
+    	// Call getCustomer() 		
+        Customer customerObj = null;
+        customerObj = Controller.getCustomer(custPhone);
+        System.out.println("The name of Customer is : " + customerObj.getFirstname() + " " + customerObj.getLastname() );
+        
+    	// Call getCustomerAddress()
+        CustomerAddress customerAddressObj = null;
+        customerAddressObj = Controller.getCustomerAddress(custPhone);
+        System.out.println("The Customer's City address is " + customerAddressObj.getCustomerCity() );
+        
+        // Call getAllCustomers() and display the Customers List
+        customerList = Controller.getAllCustomers();        
+        for (int i=0; i<customerList.size(); i++) {
+        	System.out.println("Customers in the customerList : " + customerList.get(i).getFirstname());
+        }
+
+    ////// Call and Test : Stock, Item, Order related handlers().....      
+    //////     
+    
+        
 	}
-	
-	
     
 }
